a42f670ada744c3774d38a53798df81591f91dd0
Santiago Garcia <102694038+Santy1422@users.noreply.github.com>
2023-11-09 19:43:44 +0100

    uodate

--- Whatsapp-MicroService/src/services/peticion.js
+++ Whatsapp-MicroService/src/services/peticion.js
@@ -10,16 +10,14 @@ const openai = new OpenAI({
     try{
 
     const response = await openai.createChatCompletion({
-        model: "gpt-3.5-turbo",
+        model: "gpt-4",
         messages: [{ role: "user", content: prompt }],
         temperature: 1.0,
-        max_tokens: 2000,
-        top_p: 1.0,
-        stop: ["You:"],
-        n: 1,
+        max_tokens: 3000,
       });
       return response.data.choices[0].message.content
     }catch(err) {
+      console.log(err)
       return  "Ups, disculpa, mis circuitos han fallado"
     }
   }
